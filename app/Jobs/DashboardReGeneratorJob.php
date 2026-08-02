15de3e0fab064caec9364fcc799942e196718bcc
Azim Raupov <zmraupov@gmail.com>
2026-08-02 11:00:02 +0500

    Fix generating

--- app/Jobs/DashboardReGeneratorJob.php
+++ app/Jobs/DashboardReGeneratorJob.php
@@ -2,7 +2,11 @@
 
 namespace App\Jobs;
 
+use App\Events\DashboardWidgetChanged;
+use App\Events\MessageTasksChanged;
 use App\Helpers\Dashboard\DashboardReGenerator;
+use App\Helpers\Widget\ReviewWidgetsDashboard;
+use App\Models\AiChatTask;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 
@@ -35,5 +39,36 @@ class DashboardReGeneratorJob implements ShouldQueue
         $d=new DashboardReGenerator($this->dashboardId,$this->chatId,$this->messageId);
         $d->determineChanges($this->text);
         $d->applyChanges();
+
+        $review = new ReviewWidgetsDashboard($d->newDashboard->id, $d->dataSource->id);
+
+        $task = AiChatTask::query()->create([
+            'chat_id' => $this->chatId,
+            'message_id' => $this->messageId,
+            'task_id' => $d->tasks['review_and_correction_widgets'],
+            'status_id' => $d->tasks_statuses['in_progress'],
+        ]);
+        $task->load(['status', 'task']);
+
+        $d->newDashboard->status = 'reviewing';
+        $d->newDashboard->save();
+
+        event(new MessageTasksChanged($d->message, $task,  $d->newDashboard->id));
+        event(new DashboardWidgetChanged($d->newDashboard));
+
+        $result = $review->handle();
+
+
+        $task->status_id = $d->tasks_statuses['completed'];
+        $task->save();
+        $task->load('status');
+        $d->message->status="answered";
+
+        event(new MessageTasksChanged($d->message, $task, $d->newDashboard->id));
+
+        $d->dashboard->status = 'completed';
+        $d->dashboard->save();
+        event(new DashboardWidgetChanged($d->newDashboard));
+
     }
 }
